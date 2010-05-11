pawelz/nagios-check_poldek

commit 5e5975114e905acd0457cfc80dd28bac50815317

unified stdout

check_poldek.py
@@ -104,15 +104,15 @@ for line in p.stdout:
 		resultLine = line
 
 if (numberOfErrors > 0):
-	finish ("ERROR", str(numberOfErrors) + " poldek errors: " + lasterror + " and " + str(numberOfWarns) + " poldek warnings.")
+	finish ("POLDEK ERROR", str(numberOfErrors) + " poldek errors: " + lasterror + " and " + str(numberOfWarns) + " poldek warnings.")
 
 if (numberOfPackages >= config["errorLevel"]):
-	finish ("ERROR", resultLine)
+	finish ("POLDEK ERROR", resultLine)
 
 if (numberOfWarns > 0):
-	finish ("WARNING", str(numberOfWarns) + " poldek warnings: " + lastwarns)
+	finish ("POLDEK WARNING", str(numberOfWarns) + " poldek warnings: " + lastwarns)
 
 if (numberOfPackages >= config["warningLevel"]):
-	finish ("WARNING", resultLine)
+	finish ("POLDEK WARNING", resultLine)
 
-finish ("OK", resultLine)
+finish ("POLDEK OK", resultLine)

commit 17b9eafa4094bf356a9159cb802c9668f9d47cd3

return correct rv
it ifxes regression introduced in 5e5975114e905acd0457cfc80dd28bac50815317

check_poldek.py
@@ -34,7 +34,7 @@ config = {"errorLevel":	10,
 		"verbose":		False,
 		"cache":		"/tmp/check_poldek"}
 
-result = {"OK": 0, "WARNING": 1, "ERROR": 2}
+result = {"POLDEK OK": 0, "POLDEK WARNING": 1, "POLDEK ERROR": 2}
 
 for n in range(len(sys.argv)):
 	if (sys.argv[n] == "-v"):