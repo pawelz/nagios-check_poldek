ef1ae3a156c05c2fb36fdcdbaaf3b08d578065e4
Paweł Zuzelski <pawelz@pld-linux.org>
2010-05-14 13:43:49 +0200

    Allow to pass extra args to poldek.

--- check_poldek.py
+++ check_poldek.py
@@ -32,7 +32,8 @@ copyright = "2009, 2010 TouK sp. z o.o. s.k.a."
 config = {"errorLevel":	10,
 		"warningLevel":	5,
 		"verbose":		False,
-		"cache":		"/tmp/check_poldek"}
+		"cache":		"/tmp/check_poldek",
+		"extraArgs":	[]}
 
 result = {"POLDEK OK": 0, "POLDEK WARNING": 1, "POLDEK ERROR": 2}
 
@@ -45,6 +46,9 @@ for n in range(len(sys.argv)):
 		config["warningLevel"] = int(sys.argv[n+1])
 	if (sys.argv[n] == "--cache"):
 		config["cache"] = sys.argv[n+1]
+	if (sys.argv[n] == "--"):
+		config["extraArgs"] = sys.argv[n+1:]
+		berak
 
 # Functions
 def version():
@@ -55,13 +59,14 @@ def usage():
 	version()
 	print
 	print "check_poldek.py [-w WARN] [-c ERROR] [--cache DIRECTORY]"
+	print "                [-- arguments passed to poldek]"
 	print
 
 def finish(rv, line):
 	print rv + ": " + line.splitlines()[0]
 	sys.exit(result[rv])
 
-rv=subprocess.call(["poldek", "--cache", config["cache"], "-q", "--up"],
+rv=subprocess.call(["poldek", "--cache", config["cache"], "-q", "--up"] + config["extraArgs"],
 		stderr=subprocess.STDOUT,
 		stdout=subprocess.PIPE)
 
@@ -71,7 +76,7 @@ if rv < 0:
 if rv > 0:
 	finish ("ERROR", "Could not update poldek indices: Poldek error " + str(-rv) + ".")
 
-p=subprocess.Popen(["poldek", "--cache", config["cache"], "-t", "--noask", "--upgrade-dist"],
+p=subprocess.Popen(["poldek", "--cache", config["cache"], "-t", "--noask", "--upgrade-dist"] + config["extraArgs"],
 		stderr=subprocess.STDOUT,
 		stdout=subprocess.PIPE)
 
