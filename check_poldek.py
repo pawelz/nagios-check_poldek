pawelz/nagios-check_poldek

commit ef1ae3a156c05c2fb36fdcdbaaf3b08d578065e4

Allow to pass extra args to poldek.

check_poldek.py
@@ -32,7 +32,8 @@ copyright = "2009, 2010 TouK sp. z o.o. s.k.a."
 config = {"errorLevel":	10,
 		"warningLevel":	5,
 		"verbose":		False,
-		"cache":		"/tmp/check_poldek"}
+		"cache":		"/tmp/check_poldek",
+		"extraArgs":	[]}
 
 result = {"POLDEK OK": 0, "POLDEK WARNING": 1, "POLDEK ERROR": 2}
 
@@ -45,6 +46,9 @@ for n in range(len(sys.argv)):
 		config["warningLevel"] = int(sys.argv[n+1])
 	if (sys.argv[n] == "--cache"):
 		config["cache"] = sys.argv[n+1]
+	if (sys.argv[n] == "--"):
+		config["extraArgs"] = sys.argv[n+1:]
+		berak
 
 # Functions
 def version():
@@ -55,13 +59,14 @@ def usage():
 	version()
 	print
 	print "check_poldek.py [-w WARN] [-c ERROR] [--cache DIRECTORY]"
+	print "                [-- arguments passed to poldek]"
 	print
 
 def finish(rv, line):
 	print rv + ": " + line.splitlines()[0]
 	sys.exit(result[rv])
 
-rv=subprocess.call(["poldek", "--cache", config["cache"], "-q", "--up"],
+rv=subprocess.call(["poldek", "--cache", config["cache"], "-q", "--up"] + config["extraArgs"],
 		stderr=subprocess.STDOUT,
 		stdout=subprocess.PIPE)
 
@@ -71,7 +76,7 @@ if rv < 0:
 if rv > 0:
 	finish ("ERROR", "Could not update poldek indices: Poldek error " + str(-rv) + ".")
 
-p=subprocess.Popen(["poldek", "--cache", config["cache"], "-t", "--noask", "--upgrade-dist"],
+p=subprocess.Popen(["poldek", "--cache", config["cache"], "-t", "--noask", "--upgrade-dist"] + config["extraArgs"],
 		stderr=subprocess.STDOUT,
 		stdout=subprocess.PIPE)
 

README
@@ -2,8 +2,11 @@ check_poldek is nagios plugin for monitoring systems using poldek as package
 manager (PLD Linux, or any rpm-based system using poldek). It checks if new
 system updates are available.
 
-commandline options:
+usage:
 
+poldek [options] [-- extra arguments passed directly to poldek]
+
+available options:
   -v          - be verbose (prints errors on stderr)
   -c N        - report error if there are >= N packages to install
   -w N        - report warning if there are >= N packages to install
@@ -12,7 +15,7 @@ commandline options:
 example configuration:
 
   nrpe.cfg:
-    command[check_poldek]=/usr/lib/nagios/plugins/check_poldek --cache /var/cache/check_poldek -c 10 -w 5
+    command[check_poldek]=/usr/lib/nagios/plugins/check_poldek --cache /var/cache/check_poldek -c 10 -w 5 -- -n th
   
   services.cfg:
     define service {

commit 4651619a87839b1866e17634456049f8edca0089

syntax typo
it fixes bug introduced by ef1ae3a156c05c2fb36fdcdbaaf3b08d578065e4

check_poldek.py
@@ -73,7 +73,7 @@ for n in range(len(sys.argv)):
 		config["cache"] = sys.argv[n+1]
 	if (sys.argv[n] == "--"):
 		config["extraArgs"] = sys.argv[n+1:]
-		berak
+		break
 
 rv=subprocess.call(["poldek", "--cache", config["cache"], "-q", "--up"] + config["extraArgs"],
 		stderr=subprocess.STDOUT,